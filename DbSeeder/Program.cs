2b5849cb9115715ede295c26d82eca1c6d0be378
daniel <dansanb@gmail.com>
2025-07-25 20:43:41 -0700

    Add DbSeeder project to the solution.
    Implement basic program Main() function to use the Core AppDbContext.

---
+++ DbSeeder/Program.cs
@@ -0,0 +1,18 @@
+﻿using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbSeeder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=simplelegal;Username=postgres");
+
+        using (var context = new AppDbContext(optionsBuilder.Options))
+        {
+
+        }
+    }
+}
