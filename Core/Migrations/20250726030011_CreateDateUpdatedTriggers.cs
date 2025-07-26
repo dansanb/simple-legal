b0a94f7c1c1833bdf88d12b3f498c0db831c9df9
daniel <dansanb@gmail.com>
2025-07-25 20:21:44 -0700

    Add migration to create a postgresql function 'set_updated_timestamp' which updates the DateUpdated field with the current timestamp.
    Add migration to create update trigger 'update_timestamp_trigger' on all tables.
    
    Add migration to fix 'set_updated_timestamp' postgresql function.

---
+++ Core/Migrations/20250726030011_CreateDateUpdatedTriggers.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDateUpdatedTriggers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""Cases""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+            migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""CaseNotes""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+            migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""CaseRoles""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+             migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""CaseStatusRoles""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+             migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""CasePartyTags""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+            migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""CasePartyTagRoles""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+             migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""Parties""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+
+            migrationBuilder.Sql(@"
+                 CREATE TRIGGER update_timestamp_trigger
+                 BEFORE UPDATE ON ""PartyRoles""
+                 FOR EACH ROW
+                 EXECUTE FUNCTION set_updated_timestamp();");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql(@"
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""CaseNotes"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""Cases"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""CaseRoles"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""CaseStatusRoles"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""CasePartyTags"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""CasePartyTagRoles"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""Parties"";
+                DROP TRIGGER IF EXISTS update_timestamp_trigger ON ""PartyRoles"";
+            ");
+        }
+    }
+}
---
+++ Core/Migrations/20250726031603_FixSetUpdatedTimestampFunction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSetUpdatedTimestampFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE OR REPLACE FUNCTION set_updated_timestamp()
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    NEW.DateUpdated = NOW();
+                    RETURN NEW;
+                END;
+                $$ LANGUAGE plpgsql;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
